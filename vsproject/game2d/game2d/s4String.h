fc1188b301f965733c4afe60424eb3d257794a22
Kvr0 <95079956+Kvr0@users.noreply.github.com>
2022-02-12 10:58:49 +0900

    Add/s4String

---
+++ vsproject/game2d/game2d/s4String.h
@@ -0,0 +1,30 @@
+#pragma once
+#ifndef _GAME2D_S4STRING_H_
+#define _GAME2D_S4STRING_H_
+#include <string>
+namespace game2d
+{
+#ifdef UNICODE
+	using tstring = std::wstring;
+	using tchar = wchar_t;
+#else
+	using tstring = std::string;
+	using tchar = char;
+#endif // UNICODE
+
+	class s4String : public tstring
+	{
+	public:
+		using tstring::tstring;
+		s4String(const tstring& _str)
+			:tstring(_str) {}
+	};
+}
+
+game2d::s4String operator""_s(const game2d::tchar* _str, std::size_t _count)
+{
+	return game2d::s4String(_str, _count);
+}
+
+
+#endif // !_GAME2D_S4STRING_H_
