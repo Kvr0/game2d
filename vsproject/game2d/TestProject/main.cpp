39a9f4bcf18288e1b0fdcc63e9036cb3a31377c1
Kvr0 <95079956+Kvr0@users.noreply.github.com>
2022-02-12 15:01:45 +0900

    Add/s4Matrix/s4Vector

--- vsproject/game2d/TestProject/main.cpp
+++ vsproject/game2d/TestProject/main.cpp
@@ -2,13 +2,13 @@
 #include "s4DxLib.h"
 #include "s4String.h"
 #include "s4BaseObject.h"
+#include "s4Matrix.h"
 
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_  HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd) 
 {
+	auto vec0 = game2d::make_s4Vector(640, 480);
 
-	game2d::s4String title = L"Hello"_s + L"World"_s;
-
-	if (!game2d::s4DxLib::init(640,480, title))return -1;
+	if (!game2d::s4DxLib::init(vec0.at(0), vec0.at(1), L"Window"_s))return -1;
 
 	while (game2d::s4DxLib::loop());
 
---
+++ vsproject/game2d/game2d/s4Matrix.h
@@ -0,0 +1,162 @@
+#pragma once
+#ifndef _GAME2D_S4MATRIX_H_
+#define _GAME2D_S4MATRIX_H_
+#include "s4BaseObject.h"
+#include <initializer_list>
+#include <array>
+namespace game2d
+{
+	template<typename T, size_t S> class s4ColumnVector;
+	template<typename T, size_t S> class s4RowVector;
+
+	template<typename T, size_t W, size_t H>
+	class s4Matrix : public s4BaseObject
+	{
+	private:
+		static inline bool ___ = s4BaseObject::apply<s4Matrix, s4BaseObject>();
+	public:
+		using value_type = T;
+		static inline const size_t width = W;
+		static inline const size_t height = H;
+	public:
+		std::array<value_type, W* H> data;
+	public:
+
+		template<typename ...T>
+		s4Matrix(const T& ...ts)
+			:data{static_cast<const value_type&>(ts)...} {}
+		s4Matrix()
+			:s4Matrix({}) {}
+
+		// 取得
+		constexpr value_type& at(const s4Matrix<size_t, 2, 1>& _pos)
+		{
+			return data.at(_pos.data.at(0) + width * _pos.data.at(1));
+		}
+		constexpr const value_type& at(const s4Matrix<size_t, 2, 1>& _pos) const
+		{
+			return data.at(_pos.data.at(0) + width * _pos.data.at(1));
+		}
+
+		constexpr value_type& operator[](const s4Matrix<size_t, 2, 1>& _pos)
+		{
+			return at(_pos);
+		}
+		constexpr const value_type& operator[](const s4Matrix<size_t, 2, 1>& _pos) const
+		{
+			return at(_pos);
+		}
+
+		// 行列の大きさ
+		constexpr s4Matrix<size_t, 2, 1> size() const noexcept
+		{
+			return { width, height };
+		}
+
+		// 特定の値で埋める
+		constexpr void fill(const value_type& _u)
+		{
+			data.fill(_u);
+		}
+
+		// 行ベクトルの取得
+		constexpr s4ColumnVector<value_type, width> getColumn(size_t _j) const
+		{
+			s4ColumnVector<value_type, width> tmp;
+			for (size_t i = 0; i < width; i++) tmp.at(i) = at(i, _j);
+			return tmp;
+		}
+
+		// 列ベクトルの取得
+		constexpr s4RowVector<value_type, height> getRow(size_t _i) const
+		{
+			s4RowVector<value_type, height> tmp;
+			for (size_t j = 0; j < height; j++) tmp.at(j) = at(_i, j);
+			return tmp;
+		}
+	};
+
+	// 行ベクトル
+	template<typename T, size_t S>
+	class s4ColumnVector : public s4Matrix<T, S, 1>
+	{
+	public:
+		using value_type = T;
+	private:
+		static inline bool ___ = s4BaseObject::apply<s4Matrix<value_type, S, 1>>();
+	public:
+		using s4Matrix<value_type, S, 1>::s4Matrix;
+
+		// 取得
+		constexpr value_type& at(size_t _idx)
+		{
+			return s4Matrix<value_type, S, 1>::at({ _idx, 0 });
+		}
+		constexpr const value_type& at(size_t _idx) const
+		{
+			return s4Matrix<value_type, S, 1>::at({ _idx, 0 });
+		}
+
+		constexpr value_type& operator[](size_t _idx)
+		{
+			return at(_idx);
+		}
+		constexpr const value_type& operator[](size_t _idx) const
+		{
+			return at(_idx);
+		}
+	};
+
+	// 列ベクトル
+	template<typename T, size_t S>
+	class s4RowVector : public s4Matrix<T, 1, S>
+	{
+	public:
+		using value_type = T;
+	private:
+		static inline bool ___ = s4BaseObject::apply<s4Matrix<value_type, 1, S>>();
+	public:
+		using s4Matrix<value_type, 1, S>::s4Matrix;
+
+		// 取得
+		constexpr value_type& at(size_t _idx)
+		{
+			return s4Matrix<value_type, 1, S>::at({ _idx, 0 });
+		}
+		constexpr const value_type& at(size_t _idx) const
+		{
+			return s4Matrix<value_type, 1, S>::at({ _idx, 0 });
+		}
+
+		constexpr value_type& operator[](size_t _idx)
+		{
+			return at(_idx);
+		}
+		constexpr const value_type& operator[](size_t _idx) const
+		{
+			return at(_idx);
+		}
+	};
+
+	template<typename T, size_t S>
+	using s4Vector = s4ColumnVector<T,S>;
+
+
+	// ベクトルの作成
+	template<typename T, typename ...Args, size_t S = sizeof...(Args) + 1>
+	s4ColumnVector<T, S> make_s4ColumnVector(T _arg0, Args ...args)
+	{
+		return s4ColumnVector<T, S>({ _arg0, args... });
+	}
+	template<typename T, typename ...Args, size_t S = sizeof...(Args) + 1>
+	s4RowVector<T, S> make_s4RowVector(T _arg0, Args ...args)
+	{
+		return s4Vector<T, S>({ _arg0, args... });
+	}
+	template<typename T, typename ...Args, size_t S = sizeof...(Args) + 1>
+	s4Vector<T, S> make_s4Vector(T _arg0, Args ...args)
+	{
+		return s4Vector<T, S>({ _arg0, args... });
+	}
+}
+#endif // !_GAME2D_S4MATRIX_H_
