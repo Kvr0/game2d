5d97c2c6c3b734e81ac131524e82e345330436d0
Kvr0 <95079956+Kvr0@users.noreply.github.com>
2022-02-13 03:01:37 +0900

    Add/toString

--- vsproject/game2d/game2d/s4String.h
+++ vsproject/game2d/game2d/s4String.h
@@ -3,14 +3,23 @@
 #define _GAME2D_S4STRING_H_
 #include "s4BaseObject.h"
 #include <string>
+#include <sstream>
 namespace game2d
 {
 #ifdef UNICODE
 	using tstring = std::wstring;
 	using tchar = wchar_t;
+	using tstringstream = std::wstringstream;
+	using tistream = std::wistream;
+	using tostream = std::wostream;
+#define T(x)      L ## x
 #else
 	using tstring = std::string;
 	using tchar = char;
+	using tstringstream = std::stringstream;
+	using tistream = std::istream;
+	using tostream = std::ostream;
+#define T(x)      x
 #endif // UNICODE
 
 	class s4String : public s4BaseObject, public tstring
@@ -22,6 +31,44 @@ namespace game2d
 		s4String(const tstring& _str)
 			:tstring(_str) {}
 	};
+
+#define TSTR(x)      game2d::s4String(T(x))
+
+	template<typename V>
+	tostream& to_s(tostream& _os, const V& _v)
+	{
+		_os << _v;
+		return _os;
+	}
+
+	// •¶Žš—ñ•ÏŠ· (Stream)
+	template<typename Head>
+	void toString_s(tostream& _os, const Head& _v0)
+	{
+		to_s(_os, _v0);
+	}
+	template<typename Head, typename ...Tail>
+	void toString_s(tostream& _os, const Head& _v0, Tail... tails)
+	{
+		to_s(_os, _v0) << T(", ");
+		toString_s(_os, tails...);
+	}
+
+	// •¶Žš—ñ•ÏŠ·
+	template<typename Head>
+	s4String toString(const Head& _v0)
+	{
+		tstringstream ss;
+		toString_s(ss, _v0);
+		return ss.str();
+	}
+	template<typename Head, typename ...Tail>
+	s4String toString(const Head& _v0, Tail... tails)
+	{
+		tstringstream ss;
+		toString_s(ss, _v0, tails...);
+		return ss.str();
+	}
 }
 
 game2d::s4String operator""_s(const game2d::tchar* _str, std::size_t _count)
