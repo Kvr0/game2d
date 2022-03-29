066da7f259f4d30ce12e945e32ac6584cb4278fe
Kvr0 <95079956+Kvr0@users.noreply.github.com>
2022-03-30 02:53:28 +0900

    Update s4EventTrigger.h

--- vsproject/game2d/game2d/s4EventTrigger.h
+++ vsproject/game2d/game2d/s4EventTrigger.h
@@ -5,69 +5,124 @@
 
 namespace game2d
 {
+	// イベント
 	class s4Event : public s4BaseObject
 	{
 	private:
 		static inline bool ___ = s4BaseObject::apply<s4Event, s4BaseObject>();
 	public:
-		class EventGroup : public s4BaseObject
+		// イベントグループ
+		class Group : public s4BaseObject
 		{
 		private:
-			static inline bool ___ = s4BaseObject::apply<s4Event::EventGroup, s4BaseObject>();
+			static inline bool ___ = s4BaseObject::apply<s4Event::Group, s4BaseObject>();
 			static inline size_t __id = 0u;
-		protected:
+		public:
+			// 新規グループIDの提供
 			static constexpr size_t provide() { return __id++; }
+		public:
+			// イベントタイプ
+			class Type : public s4BaseObject
+			{
+			private:
+				static inline bool ___ = s4BaseObject::apply<s4Event::Group::Type, s4BaseObject>();
+			private:
+				size_t m_id;
+			public:
+				Type(size_t _id) :m_id(_id) {}
+
+				bool operator==(const Type& _o) const { return m_id == _o.m_id; }
+				bool operator<(const Type& _o) const { return m_id < _o.m_id; }
+			};
 		private:
 			size_t m_id;
+			size_t m_type_id;
 		public:
-			EventGroup(size_t _id) :m_id(_id) {}
+			Group(size_t _id) :m_id(_id), m_type_id(0u) {}
 
-			bool operator==(const EventGroup& _o) const { return m_id == _o.m_id; }
-			bool operator<(const EventGroup& _o) const { return m_id < _o.m_id; }
-		};
+			// 新規Typeの作成
+			Type makeType() { return Type(m_type_id++); }
 
-		class EventType : public s4BaseObject
-		{
-		private:
-			static inline bool ___ = s4BaseObject::apply<s4Event::EventType, s4BaseObject>();
-			static inline size_t __id = 0u;
-		protected:
-			static size_t provide() { return __id++; }
-		private:
-			size_t m_id;
-		public:
-			EventType(size_t _id) :m_id(_id) {}
-
-			bool operator==(const EventType& _o) const { return m_id == _o.m_id; }
-			bool operator<(const EventType& _o) const { return m_id < _o.m_id; }
+			bool operator==(const Group& _o) const { return m_id == _o.m_id; }
+			bool operator<(const Group& _o) const { return m_id < _o.m_id; }
 		};
 
 	public:
-		EventGroup egroup;
-		EventType etype;
+		Group egroup;
+		Group::Type etype;
 
-		s4Event(EventGroup _egroup, EventType _etype) :egroup(_egroup), etype(_etype) {}
+		s4Event(Group _egroup, Group::Type _etype) :egroup(_egroup), etype(_etype) {}
 	};
 
-	class s4MultiEventHandler;
+	namespace s4event
+	{
+		class s4None : public s4Event
+		{
+		private:
+			static inline bool ___ = s4BaseObject::apply<s4None, s4Event>();
+		public:
+			static inline s4Event::Group group = s4Event::Group::provide();
+			static inline s4Event::Group::Type type = group.makeType();
+
+			s4None() : s4Event(group, type) {}
+		};
+	}
 
 	class s4EventHandler : public s4BaseObject
 	{
 	private:
 		static inline bool ___ = s4BaseObject::apply<s4EventHandler, s4BaseObject>();
-	public:
-		s4EventHandler() {}
-
-		virtual void handle(const s4Event& _event) {}
-		void operator()(const s4Event& _event) { handle(_event); }
-	};
-
-	class s4MultiEventHandler : public s4EventHandler
-	{
 	private:
-		static inline bool ___ = s4BaseObject::apply<s4MultiEventHandler, s4BaseObject>();
+		static inline std::map<s4EventHandler*, std::set<s4EventHandler*>> __handler;
+		// ハンドラーの追加
+		static void add_handler(s4EventHandler& _mehandler, s4EventHandler& _ehandler)
+		{
+			__handler[&_mehandler].insert(&_ehandler);
+		}
+		static void add_handler(s4EventHandler& _mehandler)
+		{
+			__handler.try_emplace(&_mehandler);
+		}
+		// ハンドラーの削除
+		static void remove_handler(s4EventHandler& _mehandler, s4EventHandler& _ehandler)
+		{
+			__handler[&_mehandler].erase(&_ehandler);
+		}
+		static void remove_handler(s4EventHandler& _mehandler)
+		{
+			__handler.erase(&_mehandler);
+		}
+		// イベントの受け取り
+		static void receive_event(s4EventHandler& _mehandler, const s4Event& _e, s4EventHandler* _root)
+		{
+			for (auto h : __handler[&_mehandler])
+			{
+				if (h != _root) h->receiveEvent(_e, _root);
+			}
+		}
 	public:
+		s4EventHandler()
+		{
+			add_handler(*this);
+		}
+		virtual ~s4EventHandler()
+		{
+			remove_handler(*this);
+		}
 
+		virtual void addHandler(s4EventHandler& _ehandler)
+		{
+			add_handler(*this, _ehandler);
+		}
+		virtual void removeHandler(s4EventHandler& _ehandler)
+		{
+			remove_handler(*this, _ehandler);
+		}
+
+		virtual void receiveEvent(const s4Event& _e, s4EventHandler* _root = nullptr)
+		{
+			receive_event(*this, _e, _root ? _root : this);
+		}
 	};
 }
 
